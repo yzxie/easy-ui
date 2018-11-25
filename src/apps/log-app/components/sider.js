4ed669cb42cd5709e550d172e92bd36720bf166f
xieyizun <xieyizun@tigerbrokers.com>
2018-11-25 16:00:12 +0800

    日志系统侧边栏数据读取服务端接口

--- src/apps/log-app/components/sider.js
+++ src/apps/log-app/components/sider.js
@@ -9,18 +9,30 @@ export default class LogSider extends Component {
     }
 
     render() {
-        return <Sider width={200} style={{ background: '#fff' }}>
+      const { logTypeWithApps, changeActiveLogTypeAndApp } = this.props
+      
+      return <Sider width={200} style={{ background: '#fff' }}>
         <Menu
           mode="inline"
           defaultSelectedKeys={['1']}
           defaultOpenKeys={['sub1']}
           style={{ height: '100%' }}
         >
-          <SubMenu key="sub1" title={<span><Icon type="user" />核心服务</span>}>
-            <Menu.Item key="1">行情服务</Menu.Item>
-            <Menu.Item key="2">交易服务</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" title={<span><Icon type="notification" />热点股票</span>}>
+        {
+          logTypeWithApps && logTypeWithApps.map((logTypeApps, i) => (
+            <SubMenu key={i} title={<span><Icon type="user" />{logTypeApps.logType}</span>}>
+            {
+              logTypeApps.apps.map((app, j) => (
+                <Menu.Item key={j} 
+                  onClick={e => changeActiveLogTypeAndApp(logTypeApps.logType, app)}>
+                  {app}
+                </Menu.Item>
+              ))
+            }
+            </SubMenu>
+          ))
+        }
+          {/* <SubMenu key="sub2" title={<span><Icon type="notification" />热点股票</span>}>
             <Menu.Item key="3">美股</Menu.Item>
             <Menu.Item key="4">港股</Menu.Item>
           </SubMenu>
@@ -28,7 +40,7 @@ export default class LogSider extends Component {
             <Menu.Item key="5">Web</Menu.Item>
             <Menu.Item key="6">IOS</Menu.Item>
             <Menu.Item key="7">Android</Menu.Item>
-          </SubMenu>
+          </SubMenu> */}
         </Menu>
       </Sider>
     }
--- src/apps/log-app/index.js
+++ src/apps/log-app/index.js
@@ -15,36 +15,72 @@ export default class LogIndex extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            appsLogs: []
+            logTypeWithApps: [],
+            appsLogs: {},
+            activeLogType: null,
+            activeLogApp: null
         }
     }
 
-    handleLogMessage = msg => {
-        console.log('log content', msg.logContent)
-        let serverAppsLogs = msg.logContent || {}
-        let appsLogs = []
-        for (let appId in serverAppsLogs) {
-            appsLogs.push({appId: appId, logs: serverAppsLogs[appId]})
-        }
-        this.setState({
-            appsLogs: appsLogs
+    componentWillMount() {
+        axios.get("/log/list-apps").then(res => {
+            let resData = res.data
+            if (resData.ret == 0) {
+                let data = resData.data
+                this.setState({
+                    logTypeWithApps: data
+                })
+            }
+        }).catch(error => {
+            console.log(error)
         })
     }
 
+    handleLogMessage = msg => {
+        let serverAppsLogs = msg.data || null
+        if (serverAppsLogs) {
+            // logType-appId: logs
+            let appsLogs = {}
+            for (let index in serverAppsLogs) {
+                let item = serverAppsLogs[index]
+                let key = item.logType + '-' + item.app
+                let value = item.data
+                appsLogs[key] = value
+            }
+            this.setState({
+                appsLogs: appsLogs
+            })
+            console.log('appLogs: ', this.state.appsLogs)
+        }
+    }
+
+    changeActiveLogTypeAndApp = (logType, appId) => {
+        this.setState({
+            activeLogType: logType,
+            activeLogApp: appId
+        })
+    }
+
+    activeDisplayLogs = () => {
+        const { appsLogs, activeLogType, activeLogApp } = this.state
+        let key = activeLogType + '-' + activeLogApp
+        return appsLogs ? appsLogs[key] : null
+    }
+
     render() {
-        const { appsLogs } = this.state
+        const { logTypeWithApps, activeLogType, activeLogApp } = this.state
+        const displayLogs = this.activeDisplayLogs()
 
         return (
             <Layout style={{ padding: '24px 0', background: '#fff' }}>
-                <LogSider />
+                <LogSider logTypeWithApps={logTypeWithApps} 
+                    changeActiveLogTypeAndApp={this.changeActiveLogTypeAndApp} />
                 <Content style={{ padding: '0 24px', minHeight: "-webkit-fill-available" }}>
-                { 
-                    appsLogs ? appsLogs.map((appLogs, i) => (
-                        <div key={i}>
-                            <div>应用: {appLogs.appId}</div>
-                            <SecondFlowChart logs={appLogs.logs} />
-                        </div>
-                    )) : null
+                {
+                    activeLogType && activeLogApp && <div>
+                        <div>日志类型：{activeLogType}, 应用: {activeLogApp}</div>
+                        { displayLogs ? <SecondFlowChart logs={displayLogs} /> : 'No data' }
+                    </div>
                 }
                 </Content>
                 <SockJsClient
